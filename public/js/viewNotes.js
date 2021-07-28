37c4ed25fe727798b047b8515a115312c56c3d39
xiongxiongpei <xiongxiongpei@gmail.com>
2021-07-28 17:57:50 +0000

    commit trial2

---
+++ public/js/viewNotes.js
@@ -0,0 +1,52 @@
+let googleUser;
+
+window.onload = (event) => {
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+            console.log('Logged in as: ' + user.displayName);
+            googleUser = user;
+        } else {
+            window.location = 'index.html';
+        }
+    })
+}
+
+function getNotes(userId) {
+    const notesRef = firebase.database().ref(`users/${userId}`);
+    notesRef.on('value', (db) => {
+        const data = db.val();
+        renderData(data);
+    });
+}
+function renderData(data) {
+    const html = '';
+    for (const dataKey in data) {
+        const note = data[dataKey];
+        const cardHtml = renderCard(note);
+        ntml += cardHtml;
+    }
+    document.querySelector('#app').innerHTML = html;
+}
+function renderCard(note) {
+    const div = document.createElement('div');
+    div.classList.add('column', 'is-one-quarter');
+
+    const card = document.createElement('div');
+    card.classList.add('card');
+
+    div.appendChild(card);
+    //convert note to html
+    // console.log(note);
+    return `
+    <div class = "column is one quarter">
+        <div class="card">
+            <header class="card header">
+                <span class="card header title">${note.title }</span>
+            </header>
+        </div>
+        <div class="card content">
+            <div class="content">${ note.title }</div>
+        </div>
+    </div>
+    `;
+}
